8d6a7688d860841c093752091a1f9b960edec378
Fauzan Ahsan <fauzanahsan@Fauzans-MacBook-Air.local>
2012-08-28 22:18:11 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shido::Application.config.secret_token = 'cf733ce7085b2169e1191d04090c9fbdfe25a305b3d74a3037859f5b9163fb604c8039ddae46d099ad94e9e076d60aa09a4b98d9d0f1250acf294952dfd00eb3'
