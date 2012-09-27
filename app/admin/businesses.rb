aff9412abf3056bb17571409d4b33595b15099e8
Fauzan Ahsan <fauzan@Ozans.local>
2012-09-27 22:40:15 +0700

    fix role admin on business and users

--- app/admin/businesses.rb
+++ app/admin/businesses.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Business do
   menu :if => proc{ current_admin_user.has_role? ("Account Manager") }
-  menu :if => proc{ current_admin_user.has_role? ("Admin") }
+  menu :else => proc{ current_admin_user.has_role? ("Admin") }
  
   controller.authorize_resource
   
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User do
   menu :if => proc { current_admin_user.has_role?("Account Manager") } 
-  menu :if => proc { current_admin_user.has_role?("Admin") } 
+  menu :else => proc { current_admin_user.has_role?("Admin") } 
   
   controller.authorize_resource
   
