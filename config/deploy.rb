2335251678b70e891dc7591dd8dab37dfe6b6cb8
Fauzan Ahsan <fauzan@Ozans.local>
2012-10-07 21:18:16 +0700

    change deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,29 @@ ssh_options[:forward_agent] = true
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
+  namespace :assets do
+      task :precompile, :roles => :web do
+        from = source.next_revision(current_revision)
+        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+          run_locally("rake assets:clean && rake assets:precompile")
+          run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+          top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+          run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+          run_locally "rm public/assets.tar.bz2"
+          run_locally("rake assets:clean")
+        else
+          logger.info "Skipping asset precompilation because there were no asset changes"
+        end
+      end
+
+      task :symlink, roles: :web do
+        run ("rm -rf #{latest_release}/public/assets &&
+              mkdir -p #{latest_release}/public &&
+              mkdir -p #{shared_path}/assets &&
+              ln -s #{shared_path}/assets #{latest_release}/public/assets")
+      end
+    end
+    
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
@@ -54,14 +77,14 @@ namespace :deploy do
   #     end
   #   end
   
-  namespace :assets do
-      desc 'Run the precompile task locally and rsync with shared'
-      task :precompile, :roles => :web, :except => { :no_release => true } do
-        %x{bundle exec rake assets:precompile}
-        %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
-        %x{bundle exec rake assets:clean}
-      end
-    end
+  # namespace :assets do
+  #       desc 'Run the precompile task locally and rsync with shared'
+  #       task :precompile, :roles => :web, :except => { :no_release => true } do
+  #         %x{bundle exec rake assets:precompile}
+  #         %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
+  #         %x{bundle exec rake assets:clean}
+  #       end
+  #     end
   
   before "deploy", "deploy:check_revision"
 end
